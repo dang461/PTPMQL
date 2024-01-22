d301f69b0ee82b1dcda9a874399dc5452730048c
dang461 <142491780+dang461@users.noreply.github.com>
2024-01-22 16:30:38 +0700

    slide 15+19 C#_5

---
+++ DemoMvc/Models/HinhTron.cs
@@ -0,0 +1,9 @@
+namespace DemoMvc.Models
+{
+    public class Tinh
+    {
+        public double BK { get; set; }
+        public double DienTich { get; set; }
+        public const double PI = 3.14;
+    }
+}
